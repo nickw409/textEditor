6d783a453a4f019e82179172c2033a7c31490943
nickw409 <nwiley333@gmail.com>
2021-05-12 16:16:07 -0700

    Ported the editor over to c++.

---
+++ nte.cpp
@@ -0,0 +1,235 @@
+#include "nte.h"
+
+Editor::Editor(char *file)
+{
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    clear();
+
+    getmaxyx(stdscr, screen_height_, screen_width_);
+    cursor_x_ = 0;
+    cursor_y_ = 0;
+    num_lines_ = 0;
+    row_offset_ = 0;
+    file_ = file;
+}
+
+Editor::~Editor()
+{
+    endwin();
+}
+
+void Editor::GetError(std::string err)
+{
+    endwin();
+    std::cerr << err << std::endl;
+    exit(1);
+}
+
+void Editor::GetInput()
+{
+    int ch;
+
+    while ((ch = getch()) != KEY_END)
+    {
+        switch(ch)
+        {
+            case KEY_UP:
+            case KEY_DOWN:
+            case KEY_LEFT:
+            case KEY_RIGHT:
+                ProcessArrowKey(ch);
+                break;
+            case KEY_BACKSPACE:
+                DeleteChar(GetLineIndex(cursor_y_), cursor_x_);
+                RefreshScreen();
+                break;
+            default:
+                if (31 < ch < 127)
+                {
+                    EditLine(GetLineIndex(cursor_y_), cursor_x_, std::string(1, ch));
+                    ++cursor_x_;
+                    RefreshScreen();
+                }
+        }
+        move(cursor_y_, cursor_x_);
+        refresh();
+    }
+}
+
+void Editor::ProcessArrowKey(int c)
+{
+    switch(c)
+    {
+        case KEY_UP:
+            if (cursor_y_ > 0)
+            {
+                --cursor_y_;
+                cursor_x_ = GetLineLen(cursor_y_);
+            }
+            else if (cursor_y_ == 0 && row_offset_ > 0)
+            {
+                --row_offset_;
+                cursor_x_ = GetLineLen(cursor_y_);
+                RefreshScreen();
+            }
+            break;
+        case KEY_DOWN:
+            if (cursor_y_ < screen_height_ - 1)
+            {
+                ++cursor_y_;
+                cursor_x_ = GetLineLen(cursor_y_);
+            }
+            else if (cursor_y_ == screen_height_ - 1 &&
+                row_offset_ < (num_lines_ - screen_height_))
+            {
+                ++row_offset_;
+                cursor_x_ = GetLineLen(cursor_y_);
+                RefreshScreen();
+            }
+            break;
+        case KEY_LEFT:
+            if (cursor_x_ > 0)
+            {
+                --cursor_x_;
+            }
+            else
+            {
+                ProcessArrowKey(KEY_UP);
+            }
+            break;
+        case KEY_RIGHT:
+            if (cursor_x_ < GetLineLen(cursor_y_))
+            {
+                ++cursor_x_;
+            }
+            else
+            {
+                ProcessArrowKey(KEY_DOWN);
+            }
+            break;
+    }
+}
+
+void Editor::RefreshScreen()
+{
+    int line;
+    move(0, 0);
+    clear();
+    for (int i = 0; i < num_lines_; ++i)
+    {
+        line = i + row_offset_;
+        mvprintw(i, 0, "%s", lines_[line].c_str());
+    }
+    move(0, 0);
+    refresh();
+}
+
+void Editor::AddRow(std::string str)
+{
+    lines_.push_back(str);
+    ++num_lines_;
+}
+
+void Editor::AddRow(std::string str, int index)
+{
+    lines_.insert(lines_.begin()+index, str);
+    ++num_lines_;
+}
+
+void Editor::RemoveRow(int index)
+{
+    lines_.erase(lines_.begin()+index);
+    --num_lines_;
+}
+
+void Editor::EditLine(int row, int col, std::string str)
+{
+    if (row > num_lines_ || row < 0)
+    {
+        GetError("Row index for EditLine is out of bounds");
+    }
+
+    //std::string line = lines_[row];
+    if (col > GetLineLen(row) || col < 0)
+    {
+        GetError("Col index for EditLine is out of bounds");
+    }
+    lines_[row].insert(col, str);
+    //RemoveRow(row);
+    //line.insert(col, str);
+    //AddRow(line, row);
+}
+
+void Editor::DeleteChar(int row, int col)
+{
+    if (GetLineLen(row) == 0)
+    {
+        RemoveRow(row);
+        ProcessArrowKey(KEY_UP);
+    }
+    else
+    {
+        if (col > GetLineLen(row) || col < 0)
+        {
+            GetError("Col index in DeleteChar is out of bounds");
+        }
+        lines_[row].erase(col-1, 1);
+        ProcessArrowKey(KEY_LEFT);
+    }
+}
+void Editor::OpenFile()
+{
+    char c[DEFAULT_BUFFLEN];
+    std::string str;
+    std::ifstream fs;
+    fs.open(file_, std::fstream::in);
+    if ((fs.rdstate() & std::fstream::failbit) != 0)
+    {
+        fs.close();
+        GetError("Failure opening file stream.");
+    }
+    while (!fs.eof())
+    {
+        fs.getline(c, DEFAULT_BUFFLEN);
+        str = c;
+        AddRow(str);
+    }
+    fs.close();
+    RefreshScreen();
+}
+
+int Editor::GetLineIndex(int row)
+{
+    if (row + row_offset_ >= num_lines_)
+    {
+        GetError("Row in GetLineIndex out of bounds");
+    }
+    return row + row_offset_;
+}
+
+int Editor::GetLineLen(int row)
+{
+    int index = GetLineIndex(row);
+    if (index > num_lines_)
+    {
+        GetError("Row index for GetLineLen out of bounds");
+    }
+    return lines_[index].length();
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " filename" << std::endl;
+        exit(1);
+    }
+    Editor editor(argv[1]);
+    editor.OpenFile();
+    editor.GetInput();
+
+    return 0;
+}
---
+++ nte.h
@@ -0,0 +1,44 @@
+#ifndef NTE_H
+#define NTE_H
+
+#include <ncurses.h>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <vector>
+#include <fstream>
+
+#define DEFAULT_BUFFLEN 1024
+
+class Editor
+{
+    public:
+        Editor(char *file);
+        ~Editor();
+        void GetError(std::string err);
+        void GetInput();
+        void ProcessArrowKey(int c);
+        void RefreshScreen();
+        void MoveCursor(int x, int y);
+        void AddRow(std::string str);
+        void AddRow(std::string str, int index);
+        void RemoveRow(int index);
+        void EditLine(int row, int col, std::string str);
+        void DeleteChar(int row, int col);
+        void OpenFile();
+        void SaveFile();
+        int GetLineIndex(int row);
+        int GetLineLen(int row);
+    
+    private:
+        int screen_height_;
+        int screen_width_;
+        int cursor_x_;
+        int cursor_y_;
+        int num_lines_;
+        int row_offset_;
+        std::string file_;
+        std::vector<std::string> lines_;
+};
+
+#endif
